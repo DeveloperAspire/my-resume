d70723ba9a7b3e15e58d9bb0ba054834e6b4609a
Franklin <franklinokolie86@gmail.com>
2021-08-17 14:15:50 +0100

    Added Animation on Scroll to make the website beautiful

--- main.js
+++ main.js
@@ -4,5 +4,4 @@ const information =document.querySelector('.information')
 navigation.addEventListener('click', ()=>{
       navigation.classList.toggle('active')
       information.classList.toggle('active')
-      console.log('Hello')
 })
